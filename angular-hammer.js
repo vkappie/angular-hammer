cf5b268b791b9d9be59b125f9ebda23a557f5a39
Scott Boyle <scott@monospaced.co.uk>
2013-05-02 13:24:26 +0100

    make a bower component

--- angular-hammer.js
+++ angular-hammer.js
@@ -1,28 +1,10 @@
 /*
- * Angular Hammer v2
- * https://github.com/monospaced/angular-hammer
- *
- * Forked from https://github.com/randallb/angular-hammer
- * Updated to support https://github.com/EightMedia/hammer.js
- *
- * Within an Angular.js application, allows you to specify custom behaviour on Hammer.js touch events.
- *
- * Usage, currently as attribute only:
- *
- *    hm-tap="{expression}"
- *
- * You can change the default settings for the instance by adding a second attribute with options:
- *
- *    hm-options="{drag: false, transform: false}"
- *
- * Include this file, and add `hmTouchEvents` to your app's dependencies.
- *
- * Requires Hammer.js, tested with `v1.0.1 - 2013-02-26`.
- *
- * Monospaced Labs <scott@monospaced.com>
+ * angular-hammer v1.0.0
+ * (c) 2013 Monospaced http://monospaced.com
+ * License: MIT
  */
 
-(function(){
+(function(window, angular, Hammer){
 
 var hmTouchEvents = angular.module('hmTouchEvents', []),
     hmGestures = ['hmHold:hold',
@@ -50,22 +32,48 @@ angular.forEach(hmGestures, function(name){
   var directive = name.split(':'),
       directiveName = directive[0],
       eventName = directive[1];
-  hmTouchEvents.directive(directiveName, ['$parse', function($parse){
-    return function(scope, element, attr) {
-      var fn = $parse(attr[directiveName]),
-          opts = $parse(attr['hmOptions'])(scope, {}),
-          hammer = element.data('hammer');
-      if (!hammer) {
-        hammer = Hammer(element[0], opts);
-        element.data('hammer', hammer);
-      }
-      hammer.on(eventName, function(event){
-        scope.$apply(function() {
-          fn(scope, {$event: event});
+
+  hmTouchEvents.directive(directiveName, ['$parse', '$window', function($parse, $window){
+    return {
+      restrict: 'A, C',
+      link: function(scope, element, attr) {
+        var expr = $parse(attr[directiveName]),
+            fn = function(event){
+              scope.$apply(function() {
+                expr(scope, {$event: event});
+              });
+            },
+            opts = $parse(attr['hmOptions'])(scope, {}),
+            hammer;
+
+        if (typeof Hammer === 'undefined' || !$window.addEventListener) {
+          // fallback to mouse events where appropriate
+          if (directiveName === 'hmTap') {
+            element.bind('click', fn);
+          }
+          if (directiveName === 'hmDoubletap') {
+            element.bind('dblclick', fn);
+          }
+          return;
+        }
+
+        // don't create multiple Hammer instances per element
+        if (!(hammer = element.data('hammer'))) {
+          hammer = Hammer(element[0], opts);
+          element.data('hammer', hammer);
+        }
+
+        // bind Hammer touch event
+        hammer.on(eventName, fn);
+
+        // unbind Hammer touch event
+        scope.$on('$destroy', function(){
+          hammer.off(eventName, fn);
         });
-      });
+
+      }
     };
   }]);
 });
 
-})();
+})(window, window.angular, window.Hammer);
